2546e6213a5b9e379502bf9a41eb6d686d7ca147
Manav Nanwani <manav.nanwani@gmail.com>
2021-10-22 12:36:04 +0530

    Updates

--- dashboard/src/views/dashboard/Default/index.js
+++ dashboard/src/views/dashboard/Default/index.js
@@ -1,59 +1,53 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 // material-ui
-import { Grid } from '@material-ui/core';
-
+import { Grid } from "@material-ui/core";
 // project imports
-import EarningCard from './EarningCard';
-import PopularCard from './PopularCard';
-import TotalOrderLineChartCard from './TotalOrderLineChartCard';
-import TotalIncomeDarkCard from './TotalIncomeDarkCard';
-import TotalIncomeLightCard from './TotalIncomeLightCard';
-import TotalGrowthBarChart from './TotalGrowthBarChart';
-import { gridSpacing } from 'store/constant';
 
+import { gridSpacing } from "store/constant";
+import MainCard from "ui-component/cards/MainCard";
+
+import { API_SERVICE } from "../../../config";
+import axios from "axios";
 // ===========================|| DEFAULT DASHBOARD ||=========================== //
 
 const Dashboard = () => {
-    const [isLoading, setLoading] = useState(true);
-    useEffect(() => {
-        setLoading(false);
-    }, []);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
-    return (
-        <Grid container spacing={gridSpacing}>
-            <Grid item xs={12}>
-                <Grid container spacing={gridSpacing}>
-                    <Grid item lg={4} md={6} sm={6} xs={12}>
-                        <EarningCard isLoading={isLoading} />
-                    </Grid>
-                    <Grid item lg={4} md={6} sm={6} xs={12}>
-                        <TotalOrderLineChartCard isLoading={isLoading} />
-                    </Grid>
-                    <Grid item lg={4} md={12} sm={12} xs={12}>
-                        <Grid container spacing={gridSpacing}>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeDarkCard isLoading={isLoading} />
-                            </Grid>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeLightCard isLoading={isLoading} />
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={12}>
-                <Grid container spacing={gridSpacing}>
-                    <Grid item xs={12} md={8}>
-                        <TotalGrowthBarChart isLoading={isLoading} />
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <PopularCard isLoading={isLoading} />
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-    );
+  const [appts, setAppts] = useState([]);
+  const getAppts = async () => {
+    await axios
+      .get(`${API_SERVICE}/getappt`)
+      .then((res) => {
+        let sample = [];
+        res.data.map((data) =>
+          sample.push({ title: data.note, date: data.apptDate.split("T")[0] })
+        );
+        setAppts(sample);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getAppts();
+  }, []);
+
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <MainCard>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={appts}
+          />
+        </MainCard>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Dashboard;
